47dc324c1102f9f2aa6b4134702c581fdf0614d7
Shahvir <shahvirb@gmail.com>
2018-12-04 19:52:36 -0600

    added day 1 part 2

--- day1.py
+++ day1.py
@@ -1,9 +1,24 @@
 import inputreader
 
-def solve(input):
+def part1(input):
     return sum([int(n) for n in input.splitlines()])
+
+
+def part2(input):
+    sum = 0
+    freqs = set([sum])
+
+    while True: #TODO not very safe if frequency doesn't repeat
+        for line in input.splitlines():
+            sum += int(line)
+            if sum in freqs:
+                return sum
+            else:
+                freqs.add(sum)
+    return None
 
 
 if __name__ == "__main__":
     input = inputreader.read2018('day1.txt')
-    print(solve(input))
+    print(part1(input))
+    print(part2(input))
