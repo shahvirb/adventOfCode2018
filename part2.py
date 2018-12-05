6ad4c200eff39062eaa2843c86d0fdb383c044a3
Shahvir <shahvirb@gmail.com>
2018-12-04 20:12:30 -0600

    adding day 2 part 1

---
+++ part2.py
@@ -0,0 +1,29 @@
+import inputreader
+
+def checksum(code):
+    counts_dict = {}
+    for c in code:
+        if c not in counts_dict:
+            counts_dict[c] = 1
+        else:
+            counts_dict[c] += 1
+    return counts_dict
+
+
+
+def part1(input):
+    twos = 0
+    threes = 0
+    for line in input.splitlines():
+        csum = checksum(line)
+        counts = list(csum.values())
+        if 2 in counts:
+            twos += 1
+        if 3 in counts:
+            threes += 1
+    return twos * threes
+
+
+if __name__ == "__main__":
+    input = inputreader.read2018('day2.txt')
+    print(part1(input))
