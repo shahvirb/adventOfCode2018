77e56a72654dc99c1e17d12db9b37b300847bde9
Shahvir <shahvirb@gmail.com>
2018-12-05 18:44:33 -0600

    fixing register.py

--- register.py
+++ register.py
@@ -1,8 +1,8 @@
-y2018 = __import__('2018')
+import day1
+import day2
 
 REGISTRATION = [
-
-    ('2018.1.1', y2018.day1.part1),
-    ('2018.1.2', y2018.day1.part2),
-    ('2018.2.1', y2018.day2.part1),
+    ('2018.1.1', day1.part1),
+    ('2018.1.2', day1.part2),
+    ('2018.2.1', day2.part1),
 ]
