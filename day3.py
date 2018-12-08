411486c23e5a358b7f2817a16c42a36bdd9470ab
Shahvir <shahvirb@gmail.com>
2018-12-08 15:25:52 -0600

    adding day 3 part 2

--- day3.py
+++ day3.py
@@ -2,6 +2,8 @@ import inputreader
 import numpy
 import re
 
+WIDTH = 1000
+HEIGHT = 1000
 
 class Claim:
     def __init__(self, descriptor):
@@ -15,28 +17,52 @@ class Claim:
 
 
 def part1(input):
+    def make_grid(claims):
+        # find the bounding grid size
+        # for c in claims:
+        #     width = max(width, c.x + c.w)
+        #     height = max(height, c.y + c.h)
+
+        # fill grid
+        grid = numpy.zeros([WIDTH, HEIGHT])
+        for c in claims:
+            grid[c.x:c.x + c.w, c.y:c.y + c.h] += numpy.ones([c.w, c.h])
+        return grid
+
     claims = [Claim(line) for line in input.splitlines()]
-
-    #find the bounding grid size
-    #TODO is this necessary?
-    width = 0
-    height = 0
-    for c in claims:
-        width = max(width, c.x + c.w)
-        height = max(height, c.y + c.h)
-
-    #fill grid
-    grid = numpy.zeros([width, height])
-    for c in claims:
-        grid[c.x:c.x+c.w, c.y:c.y+c.h] += numpy.ones([c.w, c.h])
+    grid = make_grid(claims)
 
     #count all the non 0 and non 1 values
     discount = (grid == 0).sum() + (grid == 1).sum()
-    result = width * height - discount
+    result = WIDTH * HEIGHT - discount
     return result
 
+
+def part2(input):
+    def make_grid(claims):
+        grid = numpy.full((WIDTH, HEIGHT), object)
+        grid[...] = [[[] for _ in row] for row in grid]
+        for c in claims:
+            for i in range(c.x, c.x + c.w):
+                for j in range(c.y, c.y + c.h):
+                    grid[i, j].append(c.id)
+        return grid
+
+    claims = [Claim(line) for line in input.splitlines()]
+    grid = make_grid(claims)
+
+    for c in claims:
+        overlaps = False
+        for i in range(c.x, c.x + c.w):
+            for j in range(c.y, c.y + c.h):
+                if len(grid[i, j]) != 1:
+                    overlaps = True
+        if not overlaps:
+            return c.id
+
+    return None
 
 if __name__ == "__main__":
     input = inputreader.read2018('day3.txt')
     print(part1(input))
-    #print(part2(input))
+    print(part2(input))
