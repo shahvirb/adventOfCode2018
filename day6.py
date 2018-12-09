556b7f9fb93a7ed3b65d6b4ee1f7077028566736
Shahvir <shahvirb@gmail.com>
2018-12-09 11:39:31 -0600

    adding day 6 part 1 although it's a very slow implementation

---
+++ day6.py
@@ -0,0 +1,51 @@
+import inputreader
+import numpy
+
+def distance(a, b):
+    return sum(numpy.abs(b - a))
+
+
+def parse_line(line):
+    vals = line.split(', ')
+    return numpy.array([int(v) for v in vals])
+
+def make_grid(capitals):
+    def calc_bounds(capitals):
+        vals = numpy.array([c[1] for c in capitals]).transpose()
+        bounds = [max(v) - min(v) for v in vals]
+        offset = [min(v) for v in vals]
+        return offset, bounds
+
+    offset, bounds = calc_bounds(capitals)
+    # Adjust the capital coordinates to account for offset
+    capitals = [(c[0], c[1] - offset) for c in capitals]
+    counts = {c[0]:0 for c in capitals}
+    edges = set()
+    #grid = numpy.full(bounds, -1)
+    for x in range(bounds[0]):
+        x_edge = (x == 0 or x == bounds[0]-1)
+        for y in range(bounds[1]):
+            y_edge = (y == 0 or y == bounds[1]-1)
+            distances = [(cap[0], distance([x, y], cap[1])) for cap in capitals]
+            distances.sort(key=lambda x: x[1])
+            if distances[0][1] != distances[1][1]:
+                #grid[x, y] = distances[0][0]
+                counts[distances[0][0]] += 1
+                if x_edge or y_edge:
+                    edges.add(distances[0][0])
+
+    return counts, edges
+
+
+def part1(input):
+    capitals = [(id, parse_line(line)) for (id, line) in enumerate(input.splitlines())]
+    counts, edges = make_grid(capitals)
+    valid_states = {c:counts[c] for c in counts if c not in edges}
+    biggest = max(valid_states, key=lambda x: valid_states[x])
+    return valid_states[biggest]
+
+
+if __name__ == "__main__":
+    input = inputreader.read2018('day6.txt')
+    print(part1(input))
+    #print(part2(input))
--- register.py
+++ register.py
@@ -3,6 +3,8 @@ import day2
 import day3
 import day4
 import day5
+import day6
+
 
 REGISTRATION = [
     ('2018.1.1', day1.part1),
@@ -15,4 +17,5 @@ REGISTRATION = [
     ('2018.4.2', day4.part2),
     ('2018.5.1', day5.part1),
     ('2018.5.2', day5.part2),
+    ('2018.6.1', day6.part1),
 ]
