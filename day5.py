df33ea6ce19cba702c6d38a5d04330b0485bfcb3
Shahvir <shahvirb@gmail.com>
2018-12-12 18:38:13 -0600

    much faster implementation. Don't make a list of strings for each char, just operate directly on strings.

--- day5.py
+++ day5.py
@@ -11,28 +11,28 @@ def react(sequence):
         curr_char = sequence[i]
         next_char = sequence[i + 1]
         if reacts(curr_char, next_char):
-            del sequence[i]
-            del sequence[i]
-            i -= 1
+            sequence = sequence[:i] + sequence[i+2:]
+            if i != 0: #Don't back-track
+                i -= 1
         else:
             i += 1
     return len(sequence) #, ''.join(input)
 
 
 def part1(input):
-    input = list(input.strip())
-    return react(input)
+    return react(input.strip())
 
 
 def part2(input):
-    input = list(input.strip())
+    input = input.strip()
     units = set([u.upper() for u in input])
     counts = []
     for unit in units:
-        sequence = [i for i in input if (i != unit and i != unit.lower())]
+        sequence = input.replace(unit, '').replace(unit.lower(), '')
         reacted = react(sequence)
         counts.append(reacted)
     return min(counts)
+
 
 if __name__ == "__main__":
     input = inputreader.read2018('day5.txt')
